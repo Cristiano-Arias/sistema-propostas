9035078d1cbc286ec45b081999c51f238c52ae23
Cristiano-Arias <cristianoarias@hotmail.com>
2025-08-14 17:08:04 -0300

    Delete db_migration_add_cnpj.sql

--- db_migration_add_cnpj.sql
+++
@@ -1,4 +0,0 @@
--- Executar no seu banco (ajuste conforme SGBD)
-ALTER TABLE usuarios ADD COLUMN IF NOT EXISTS cnpj TEXT;
--- Em Postgres: CREATE UNIQUE INDEX IF NOT EXISTS idx_usuarios_cnpj_unique ON usuarios(cnpj) WHERE cnpj IS NOT NULL;
--- Em MySQL: ALTER TABLE usuarios ADD COLUMN cnpj VARCHAR(32) UNIQUE NULL;
