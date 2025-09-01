55aeeb9563b86baae531344f08409177d02801e0
Cristiano-Arias <cristianoarias@hotmail.com>
2025-09-01 19:33:29 -0300

    Update firebase.js

--- static/js/firebase.js
+++ static/js/firebase.js
@@ -16,13 +16,14 @@ import {
 
 // ⚠️ Substituir pelas suas credenciais
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "portal-de-proposta.firebaseapp.com",
-  projectId: "portal-de-proposta",
-  storageBucket: "portal-de-proposta.appspot.com",
-  messagingSenderId: "XXXXXXXXX",
-  appId: "1:XXXXXXXX:web:XXXXXXX"
-};
+    apiKey: "AIzaSyCgF366Ft7RkZHYaZb77HboNO3BPbmCjT8",
+    authDomain: "portal-de-proposta.firebaseapp.com",
+    projectId: "portal-de-proposta",
+    storageBucket: "portal-de-proposta.firebasestorage.app",
+    messagingSenderId: "321036073908",
+    appId: "1:321036073908:web:3149b9ea2cb77a704890e1",
+    measurementId: "G-CFFVQGM3EC"
+  };
 
 // Inicializar Firebase
 const app = initializeApp(firebaseConfig);
